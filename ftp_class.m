e685267d92657532409157c13a03688abae7de6e
Maxwell Lambek <maxwell.lambek@colorado.edu>
2017-10-18 15:30:00 -0600

    Class done-ish

--- ftp_class.m
+++ ftp_class.m
@@ -6,31 +6,36 @@ classdef ftp_class
         widthHeight;
         L = 1465;
         D = 380;
-        f = 111/(45*10*2*pi);
+        f = 111/(45*10*2*pi); 
+        xmin = 2000;
+        ymin = 1500;
     end
     methods
-        function obj = ftp_class(arg1,arg2)
-            obj.refim = im2double(rgb2gray(imread(arg1)));
-            obj.dataim = im2double(rgb2gray(imread(arg2)));
+        function obj = ftp_class(addr1,addr2)
+            obj.refim = im2double(rgb2gray(imread(addr1)));
+            obj.dataim = im2double(rgb2gray(imread(addr2)));
         end
-
-        function transform(obj)
-            rim = imcrop(obj.refim,[2000 1500 obj.widthHeight obj.widthHeight]);
-            dim = imcrop(obj.dataim,[2000 1500 obj.widthHeight obj.widthHeight]);
+        function [r1,r2] = crop(obj)
+            rim = imcrop(obj.refim,[obj.xmin obj.ymin obj.widthHeight obj.widthHeight]);
+            dim = imcrop(obj.dataim,[obj.xmin obj.ymin obj.widthHeight obj.widthHeight]);
 
             rim = adapthisteq(rim,'clipLimit',0.02,'Distribution','rayleigh');
             dim = adapthisteq(dim,'clipLimit',0.02,'Distribution','rayleigh');
+            
+            [r1,r2] = filterIMG(rim,dim);
+        end
 
-            [refimFilt,dataimFilt] = filterIMG(rim,dim);
+        function transform(obj)
 
+            [refimFilt,dataimFilt] = obj.crop();
             dp = ones(obj.widthHeight+1,obj.widthHeight+1);
-            for ii = 1:size(rim,2)
+            for ii = 1:(obj.widthHeight+1)
                 dp(ii,:) = (1/2)*(-angle(ifft(fft(hilbert(refimFilt(ii,:)))))...
                     + angle(ifft(fft(hilbert(dataimFilt(ii,:))))));
             end
             tol = 1;
-            for jj = 1:size(rim,2)
-                for ii = 1:length(rim)-1
+            for jj = 1:(obj.widthHeight+1)
+                for ii = 1:obj.widthHeight
                     if abs(dp(ii + 1,jj) - dp(ii,jj)) > tol
                         dp(ii+1:end,jj) = dp(ii+1:end,jj)...
                             -  (dp(ii + 1,jj) - dp(ii,jj));
@@ -50,7 +55,7 @@ classdef ftp_class
             % 
             % [vdim, hdim] = size(h);
             
-            xPlot = linspace(0,1500/111,1501);
+            xPlot = linspace(0,obj.widthHeight/111,obj.widthHeight+1); % The 111 is the same as from the numerator on f.
             figure;
             %disp('test')
             %pause
